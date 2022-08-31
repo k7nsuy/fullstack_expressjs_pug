ad8cdc43399f6850b87bc5fa4f419536b65a4d4a
k7nsuy <76999175+k7nsuy@users.noreply.github.com>
2022-08-31 23:40:30 +0900

    middleware

--- src/index.js
+++ src/index.js
@@ -3,17 +3,34 @@ import express from "express";
 const app = express() 
 const port = 3000
 
-const handleHome = (req,res) => {
+const handleHome = (req,res) => { // controller
     console.log(`hihi`);
     return res.send(`I still love you`)
 }
 
-const handleLogin = (req,res) => {
-    return res.send(`login here`) 
+const handleProtected = (req,res) => {
+    return res.send(`Welcome to the private lounge`) 
 }
 
-app.get('/', handleHome)
-app.get('/login', handleLogin)
+const logger = (req,res,next) => { // middleware (middle soft ware)
+    console.log(`${req.method},${req.url}`);
+    next()
+}
 
+const privateMiddleware = (req,res,next) => { //privateMiddleware => middleware (middle soft ware)
+    const url = req.url
+    if(url === "/protected") {
+        return res.send("<h1>Not Allowed</h1>")
+    }
+    next()
+}
+
+app.use(logger)
+app.use(privateMiddleware)
+app.get('/', handleHome)
+app.get('/protected',handleProtected)
+
+ 
 const handleListening = () => console.log(`✅ Server listening on port ${port} 🚀`);
 app.listen(port, handleListening) // handleListening => callback function
+ 
