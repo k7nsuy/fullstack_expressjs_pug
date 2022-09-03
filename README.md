00dc6bbde987c2b2303723c8a5bf325ce6ca5cfe
k7nsuy <76999175+k7nsuy@users.noreply.github.com>
2022-09-03 16:19:09 +0900

    router

---
+++ README.md
@@ -0,0 +1,20 @@
+# Wetube Reload
+Router => mini application
+
+# Global Router
+/ -> Home
+/join -> Join
+/login -> Login
+/search -> Search
+
+# User Router
+/users/edit -> Edit user
+/users/delete -> Delete user
+
+# Video Router
+/videos/watch -> Watch Video
+/videos/edit -> Edit Video
+/videos/delete -> Delete Video
+/videos/comments -> Comment on a video
+/videos/comments/delete -> Delete A Comment of a Video
+
--- src/index.js
+++ src/index.js
@@ -1,37 +1,66 @@
 import express from "express";
-import morgan from "morgan"
+import morgan from "morgan" // morgan을 통해서 middleware을 만들 필요 없이 5가지 버전으로
+// http log에 대한 정보를 app.use를 통해 middleware 형식으로 제공
 
 const app = express() 
 const port = 3000
-const logger = morgan("combined")
+const logger = morgan("dev") // dev,combined,common,short,tiny (morgan - 5가지 데이터 형식)
+// 필요한 상황에 따라 사용
 
 
-const handleHome = (req,res) => { // controller
-    console.log(`hihi`);
-    return res.send(`I still love you`)
-}
+const globalRouter = express.Router() // Router
+const handleHome = (req,res) => res.send("home") // controller
 
-const handleProtected = (req,res) => {
-    return res.send(`Welcome to the private lounge`) 
-}
+const videoRouter = express.Router()
+const handleWatchVideo = (req,res) => res.send("Watch Video")
+
+const userRouter = express.Router()
+const handleEitUser = (req,res) => res.send("Edit user")
+
+
+
+app.use("/", globalRouter)
+app.use("/edit", videoRouter)
+app.use("/watch", userRouter)
+
+app.use(logger)
+
+globalRouter.get("/", handleHome)
+videoRouter.get("/videos", handleWatchVideo)
+userRouter.get("/users", handleEitUser)
+
+
+
+// app.get('/', handleHome)
+// app.get('/videos', handleWatchVideo)
+// app.get('/users', handleEitUser)
+
+
+
 
 // const logger = (req,res,next) => { // middleware (middle soft ware)
 //     console.log(`${req.method},${req.url}`);
 //     next()
 // }
 
-const privateMiddleware = (req,res,next) => { //privateMiddleware => middleware (middle soft ware)
-    const url = req.url
-    if(url === "/protected") {
-        return res.send("<h1>Not Allowed</h1>")
-    }
-    next()
-}
 
-app.use(logger)
-app.use(privateMiddleware)
-app.get('/', handleHome)
-app.get('/protected',handleProtected)
+
+
+// const privateMiddleware = (req,res,next) => { //privateMiddleware => middleware (middle soft ware)
+//     const url = req.url
+//     if(url === "/protected") {
+//         return res.send("<h1>Not Allowed</h1>")
+//     }
+//     next()
+// }
+
+// app.use(privateMiddleware)
+
+// const handleProtected = (req,res) => {
+//     return res.send(`Welcome to the private lounge`) 
+// }
+
+// app.get('/protected',handleProtected)
 
  
 const handleListening = () => console.log(`✅ Server listening on port ${port} 🚀`);
