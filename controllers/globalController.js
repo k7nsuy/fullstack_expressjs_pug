98d04b5985e9cf4602dce414aa5ac0e853a403c3
k7nsuy <76999175+k7nsuy@users.noreply.github.com>
2022-09-06 02:15:58 +0900

    render

--- controllers/globalController.js
+++ controllers/globalController.js
@@ -1,4 +1,4 @@
-export const home = (req,res) => res.render("home") // controller
-export const join = (req,res) => res.send("Join")
+export const home = (req,res) => res.render("home", {pageTitle: "Home"}) // 변수를 보내는 방법
+export const join = (req,res) => res.send("Join") // controller
 export const login = (req,res) => res.send("Login")
 export const search = (req,res) => res.send("Search")
