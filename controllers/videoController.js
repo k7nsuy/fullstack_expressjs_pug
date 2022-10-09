88e58af58321a19ee42591529975ac4aa5b301f3
Sangwoo Yu <76999175+k7nsuy@users.noreply.github.com>
2022-10-09 19:10:38 +0900

    edit

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -5,15 +5,24 @@ export const home = async (req,res) => {
     return res.render("home", {pageTitle: "Home", videos})
 }
 export const remove = (req,res) => res.send("remove")
-export const watch = (req,res) => {
+export const watch = async (req,res) => {
     const {id} = req.params
+    const video = await Video.findById(id)
+    if(!video) {
+        return res.render("404", {pageTitle: "Video not found."})
+    }
+    return res.render("watch", {pageTitle: video.title, video})
     
-    return res.render("watch", {pageTitle: `Watching`})
 }
-export const getEdit = (req,res) => {
+export const getEdit = async (req,res) => {
     const {id} = req.params
-    
-    return res.render("edit", {pageTitle: `Editing`})
+    const video = await Video.findById(id)
+    console.log(video.hashtags.join());
+
+    if(!video) {
+        return res.render("404", {pageTitle: "Video not found."})
+    }
+    return res.render("edit", {pageTitle: `Editing`, video})
 }
 
 export const postEdit = (req,res) => {
