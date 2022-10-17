2ddc08143e1e5dccd40e784884729a5b01d3b139
Sangwoo Yu <76999175+k7nsuy@users.noreply.github.com>
2022-10-17 21:32:51 +0900

    regex

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,10 +1,12 @@
 import Video from "../src/models/video"
 
 export const home = async (req,res) => {
-    const videos = await Video.find({})         
+    const videos = await Video.find({}).sort({createdAt:"desc"})         
     return res.render("home", {pageTitle: "Home", videos})
 }
+
 export const remove = (req,res) => res.send("remove")
+
 export const watch = async (req,res) => {
     const {id} = req.params
     const video = await Video.findById(id)
@@ -14,6 +16,7 @@ export const watch = async (req,res) => {
     return res.render("watch", {pageTitle: video.title, video})
     
 }
+
 export const getEdit = async (req,res) => {
     const {id} = req.params
     const video = await Video.findById(id)
@@ -34,9 +37,7 @@ export const postEdit = async (req,res) => {
     }
 
     await Video.findByIdAndUpdate(id, {
-        title,description,hashtags:hashtags
-        .split(",")
-        .map((word) => (word.startsWith("#")) ? word : `#${word}`)
+        title,description,hashtags: Video.formatHashtags(hashtags)
     })
         
     return res.redirect(`/videos/${id}`)
@@ -55,7 +56,7 @@ export const postUpload = async (req,res) => {
             title, // same with title:title(req.body.title)
             description,
             createdAt: Date.now(), // Select date now
-            hashtags,
+            hashtags: Video.formatHashtags(hashtags),
             // : hashtags.split(",").map((word) => `#${word}`)
             // ,를 기준으로 array 안의 data를 split을 통해 독립적으로 분리시키고
             // map을 통해 각각의 분리된 단어들에 #을 추가한다.
@@ -73,6 +74,25 @@ export const postUpload = async (req,res) => {
     }
 }
 
+export const deleteVideo = async (req,res) => {
+    const {id} = req.params
+    await Video.findByIdAndDelete(id)
+    // delete video
+    return res.redirect("/")
+}
+
+export const search = async (req,res) => {
+    const {keyword} = req.query
+    let videos = []
+    if(keyword) {
+        videos = await Video.find({
+            title: {
+                $regex: new RegExp(`${keyword}$`, "i")
+            }
+        })
+    }
+    return res.render("search", {pageTitle:"Search", videos})
+}
 
 
 
