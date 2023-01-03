3353a53ef12ce6de279653530d5b1f94d0bc92cf
Sangwoo Yu <76999175+k7nsuy@users.noreply.github.com>
2023-01-03 14:31:15 +0900

    github login

--- controllers/userController.js
+++ controllers/userController.js
@@ -66,7 +66,7 @@ export const postLogin = async(req,res) => {
 export const startGihubLogin = (req,res) => {
     const baseUrl = 'https://github.com/login/oauth/authorize'
     const config = {
-        client_id: "7964c6287293698b72f1",
+        client_id: process.env.GITHUB_CLIENT,
         allow_signup: false,
         scope: "read:user user:email",
     }
@@ -75,8 +75,24 @@ export const startGihubLogin = (req,res) => {
     return res.redirect(finalUrl)
 }
 
-export const finishGithubLogin = (req,res) => {
-    
+export const finishGithubLogin = async (req,res) => {
+    const baseUrl = "https://github.com/login/oauth/access_token"
+    const config = {
+        clinet_id: process.env.GITHUB_CLIENT,
+        client_secret: process.env.GITHUB_SECRET,
+        code: req.query.code
+    }
+    const params = new URLSearchParams(config).toString()
+    const finalUrl = `${baseUrl}?${params}`
+    const data = await fetch(finalUrl, {
+        method: "POST",
+        headers: {
+            Accept: "application/json"
+        }
+    })
+    const json = await data.json()
+    console.log(json);
+
 }
 export const edit = (req,res) => res.send("Edit")
 export const remove = (req,res) => res.send("remove")
